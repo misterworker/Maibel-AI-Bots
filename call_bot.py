ce9fa236e8de8d688b19c35adfdff74daa24a3ca
ethan <workoutwrecker@gmail.com>
2025-01-07 01:34:36 +0800

    Add PostgreSQL for checkpoints

--- call_bot.py
+++ call_bot.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse, JSONResponse
+from contextlib import asynccontextmanager
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from langchain_core.messages import HumanMessage, SystemMessage, trim_messages, AIMessage
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
+from psycopg_pool import AsyncConnectionPool
+from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.graph import START, MessagesState, StateGraph
 from pc_vs import VectorStoreManager
+from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 import os
-
 from constants import PERSONALITIES
 
 load_dotenv()
@@ -16,28 +19,47 @@ load_dotenv()
 # Constants and configuration
 NVIDIA_API_KEY = os.getenv("NVIDIA_API_KEY")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+DB_URI = os.getenv("DB_URI")
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global postgres_pool, memory, langgraph_agent
+    
+    postgres_pool = AsyncConnectionPool(
+        conninfo=DB_URI,
+        max_size=20,
+        kwargs={"autocommit": True, "prepare_threshold": 0}
+    )
+    async with postgres_pool.connection() as conn:
+        memory = AsyncPostgresSaver(conn=conn)
+        workflow.add_node("model", call_model)
+        workflow.add_edge(START, "model")
+        langgraph_agent = workflow.compile(checkpointer=memory)
+        yield
+    
+    # await memory.setup()
+    await postgres_pool.close()
+
+app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins (you can restrict this to specific origins)
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
+tavily = TavilySearchResults(max_results=3)
 
 # Setup workflow for LangChain
 workflow = StateGraph(state_schema=MessagesState)
 
-nemo_nvidia_llm = ChatNVIDIA(model="meta/llama-3.1-70b-instruct", api_key=NVIDIA_API_KEY)
+nemo_nvidia_llm = ChatNVIDIA(model="meta/llama-3.1-70b-instruct", api_key=NVIDIA_API_KEY).bind_tools([tavily])
 pinecone_vs = VectorStoreManager()
 
 async def call_model(state: MessagesState, config):
-    print("State: ", state)
-
     trimmed_state = trim_messages(state['messages'], strategy="last", token_counter=len, 
-                                  max_tokens=21, start_on="human", end_on=("human"), include_system=False) # Gets context of last 21 messages
+                                  max_tokens=21, start_on="human", end_on=("human"), include_system=False)  # Gets context of last 21 messages
     user_input = trimmed_state[-1].content
 
     retrieved_docs = pinecone_vs.retrieve_from_vector_store(user_input, 1)
@@ -48,6 +70,7 @@ async def call_model(state: MessagesState, config):
         background = config["configurable"].get("background")
         personalities = config["configurable"].get("personalities")
         gender = config["configurable"].get("gender")
+        name = config["configurable"].get("name", "Coach")
     else:
         personality_data = PERSONALITIES.get(personalityId, PERSONALITIES["female_coach"])
         background = personality_data.get("Background", "")
@@ -55,8 +78,12 @@ async def call_model(state: MessagesState, config):
         name = personality_data.get("Name", "")
         gender = personality_data.get("Gender", "")
 
-    system_prompt = f"You are {name}({gender})\nThis is your background: {background}\nUse this as contextual information:\n"
-    f"{retrieved_context}\nThese are your personalities: {personalities}\nWhen communicating with the user, remember to stay in character."
+    system_prompt = (
+        f"You are {name} ({gender}).\nThis is your background: {background}\n"
+        f"Use this as contextual information:\n{retrieved_context}\n"
+        f"These are your personalities: {personalities}\n"
+        "When communicating with the user, remember to stay in character."
+    )
 
     messages = [SystemMessage(content=system_prompt)] + trimmed_state
 
@@ -66,26 +93,13 @@ async def call_model(state: MessagesState, config):
     except Exception as e:
         raise Exception(f"stream failed: {e}")
 
-
-# Add the function to the workflow
-workflow.add_node("model", call_model)
-workflow.add_edge(START, "model")
-
-# Simple memory management
-memory = MemorySaver()
-langchainApp = workflow.compile(checkpointer=memory)
-
 @app.post("/chat")
 async def chat_endpoint(request: Request):
-    """
-    FastAPI endpoint to handle user input and generate AI responses.
-    """
     data = await request.json()
     user_input = data.get("message", "")
     userid = data.get("userid", "")
     personality = data.get("personality", "bubbly_coach")
     
-    # Validate input
     if not user_input:
         return JSONResponse(content={"error": "Message is required"}, status_code=400)
     if not userid:
@@ -94,12 +108,13 @@ async def chat_endpoint(request: Request):
     try:
         # Stream the model response back to the client
         async def message_stream():
-            config = {"configurable": {"thread_id": personality, "user_id": userid, "personality": personality}}
+            config = {"configurable": {"thread_id": userid, "personality": personality}}
             
             messages = {"messages": [HumanMessage(content=user_input)]}
             
             try:
-                async for msg, metadata in langchainApp.astream(messages, config=config, stream_mode="messages"):
+                # stream_mode should be how chunks are outputted (values = state, messages is pure content)
+                async for msg, metadata in langgraph_agent.astream(messages, config=config, stream_mode="messages"):
                     yield msg.content
                 
             except Exception as e:
@@ -108,6 +123,4 @@ async def chat_endpoint(request: Request):
         return StreamingResponse(message_stream(), media_type="text/plain")
 
     except Exception as e:
-        # Handle any errors and return an appropriate error response
         return JSONResponse(content={"error": f"Internal server error: {str(e)}"}, status_code=500)
-
